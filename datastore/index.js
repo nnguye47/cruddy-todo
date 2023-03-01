e4544af38ee23640ee506e25c34f35534cada005
Nam Nguyen <nam.nguyen201@gmail.com>
2023-02-28 16:41:11 -0800

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -81,14 +81,15 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var file = id + '.txt';
+  var fp = path.join(exports.dataDir, file);
+  fs.unlink(fp, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
